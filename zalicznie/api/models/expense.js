28c4058fe40e739d92c584b0f6f212a16ed8ebe9
maleckyy <kupferpatryk@gmail.com>
2023-11-27 19:19:59 +0100

    get/post expenses

--- zalicznie/api/models/expense.js
+++ zalicznie/api/models/expense.js
@@ -1,4 +1,8 @@
-const mongoose = require("mongoose")
-const expenseScheme = mongoose.Schema({title: String, price: Number, created: Date });
+const mongoose = require("mongoose");
+const expenseScheme = mongoose.Schema({
+  title: String,
+  price: Number,
+  created: String,
+});
 
-module.exports = mongoose.model("Expense", expenseScheme)
+module.exports = mongoose.model("Expense", expenseScheme);
--- zalicznie/api/routes/expense.js
+++ zalicznie/api/routes/expense.js
@@ -2,8 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Expenses = require("../models/expense");
 //
-router.get("/", (req, res, next) => {
-  res.status(201).json({ wiadomosc: "expense get" });
+router.get("/", (req, res) => {
+  Expenses.find().then((result) => {
+    res.status(201).json({ wiadomosc: "expense get", data: result });
+  });
+});
+// {title: String, price: Number, created: Date }
+router.post("/new", (req, res) => {
+  //
+  const newExpense = new Expenses({
+    title: req.body.title,
+    price: req.body.price,
+    created: new Date().toLocaleDateString(),
+  });
+
+  newExpense.save();
+  res.json(newExpense);
+  //
 });
 
 //
