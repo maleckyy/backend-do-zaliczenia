66141773df8d3c869265b87c0195133efa63dff3
maleckyy <kupferpatryk@gmail.com>
2023-11-28 01:50:10 +0100

    cleanup

--- zalicznie/api/routes/user.js
+++ zalicznie/api/routes/user.js
@@ -5,37 +5,39 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // REJSTRACJA
-
 router.post("/signup", (req, res, next) => {
-  User.findOne({ email: req.body.email })
-  .then((foundUser) => {
+  User.findOne({ email: req.body.email }).then((foundUser) => {
+    // sprawdzenie czy uzytkownik juz istnieje
     if (foundUser) {
-      res.status(201).json({wiadomosc:"Ten użytkownik już istnieje"});
+      res.status(201).json({ wiadomosc: "Ten użytkownik już istnieje" });
     } else {
+      // gdy uzytkownik o takim emailu nie istnieje
       bcrypt.hash(req.body.password, 10).then((hash) => {
         const user = new User({
           email: req.body.email,
           password: hash,
         });
-        user.save().then(()=> {
-            return ( res.status(201).json({wiadomosc:"Użytkownik zarejestrowany"}))
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).json("Błąd serwera");
-        });
+        user
+          .save()
+          .then(() => {
+            return res
+              .status(201)
+              .json({ wiadomosc: "Użytkownik zarejestrowany" });
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).json("Błąd serwera");
+          });
       });
     }
-  })
-
-
+  });
 });
 
 // LOGOWANIE
 router.post("/login", (req, res, next) => {
   User.findOne({ email: req.body.email }).then((user) => {
     if (!user) {
-      return res.status(404).json({ wiadomosc: "Błąd autoryzacji znalesc" });
+      return res.status(404).json({ wiadomosc: "Nie ma takiego użytkownika" });
     }
     bcrypt.compare(req.body.password, user.password, function (err, result) {
       if (result) {
--- zalicznie/server.js
+++ zalicznie/server.js
@@ -1,18 +1,13 @@
-// import http
 const http = require("http");
 
-//import opcji
 const app = require("./app");
 
-// ustawiam port
 const port = process.env.port || 3001;
 
-// tworzę serwer
 const server = http.createServer(app);
 
-// odpalam serwer
 //nodemon server.js
 
 server.listen(port, () => {
-  console.log("serwer na porcie 3001");
+  console.log(`serwer na porcie: ${port}`);
 });
